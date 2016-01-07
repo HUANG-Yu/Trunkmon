e790425d9db1ec93acebabd7f475ab000be75af9
yuHUANG <yu.huang@idt.net>
2016-01-07 15:20:18 -0500

    commit string file

--- app/src/main/java/net/idt/trunkmon/ThresholdsFilterActivity.java
+++ app/src/main/java/net/idt/trunkmon/ThresholdsFilterActivity.java
@@ -1,10 +1,13 @@
 package net.idt.trunkmon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ThresholdsFilterActivity extends AppCompatActivity {
@@ -24,6 +27,37 @@ public class ThresholdsFilterActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_content) {
+            startActivity(new Intent(this, SelectionsActivity.class));
+        } else if (id == R.id.action_violations) {
+            startActivity(new Intent(this, ViolationsFilterActivity.class));
+        } else if (id == R.id.action_thresholds) {
+            startActivity(new Intent(this, ThresholdsFilterActivity.class));
+        } else if (id == R.id.action_logout) {
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            //id == R.id.action_about
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
