4f8dc579bd1f0e087c50c681b3a20fe436cc78c6
yuHUANG <yu.huang@idt.net>
2016-01-07 16:32:14 -0500

    fab

--- app/src/main/java/net/idt/trunkmon/LoginActivity.java
+++ app/src/main/java/net/idt/trunkmon/LoginActivity.java
@@ -24,14 +24,6 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Button loginBt = (Button)findViewById(R.id.loginBt);
         loginBt.setOnClickListener(new View.OnClickListener() {
