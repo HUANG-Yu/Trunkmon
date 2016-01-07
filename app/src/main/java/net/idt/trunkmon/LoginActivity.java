efd6c5a862b071905a3d1680da938a784532802f
GloriaIce <gloriashiyy@nyu.edu>
2016-01-07 14:19:13 -0500

    1. login activity 2. intent from login to selections

--- app/src/main/java/net/idt/trunkmon/LoginActivity.java
+++ app/src/main/java/net/idt/trunkmon/LoginActivity.java
@@ -8,12 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Button;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "log message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +29,16 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button loginBt = (Button)findViewById(R.id.loginBt);
+        loginBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getApplicationContext(),SelectionsActivity.class);
+                startActivity(intent);
+                Log.i(TAG, "login button clicked");
             }
         });
     }
