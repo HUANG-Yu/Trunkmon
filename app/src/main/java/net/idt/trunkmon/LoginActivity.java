f21422479b768049a8bd01e6e4dbf98ff7901864
GloriaIce <gloriashiyy@nyu.edu>
2016-01-19 17:01:19 -0500

    Add local sqlite database to mock user login.

--- app/src/main/java/net/idt/trunkmon/LoginActivity.java
+++ app/src/main/java/net/idt/trunkmon/LoginActivity.java
@@ -16,32 +16,55 @@ import android.content.Intent;
 import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.beardedhen.androidbootstrap.BootstrapEditText;
 
+import database.UserDBHandler;
+import database.VioDBHandler;
+
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "login log message";
-
+    UserDBHandler userDbHandler;
+    MyApplication myApplication;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        userDbHandler = new UserDBHandler(this, null, null, 1);
+        myApplication =(MyApplication)getApplicationContext();
+
         Log.i(TAG, "login activity onCreate");
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BootstrapEditText et_boot_username = (BootstrapEditText)findViewById(R.id.et_boot_username);
+        final BootstrapEditText et_boot_username = (BootstrapEditText)findViewById(R.id.et_boot_username);
         et_boot_username.setRounded(true);
 
-        BootstrapEditText et_boot_password = (BootstrapEditText)findViewById(R.id.et_boot_password);
+        final BootstrapEditText et_boot_password = (BootstrapEditText)findViewById(R.id.et_boot_password);
         et_boot_password.setRounded(true);
 
         BootstrapButton btn_boot_login = (BootstrapButton)findViewById(R.id.boot_btn_login);
 
 
+
         btn_boot_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),SelectionsActivity.class);
-                Log.i(TAG, "login button clicked");
-                startActivity(intent);
+                int role = userDbHandler.checkUser(et_boot_username.getText().toString(),et_boot_password.getText().toString());
+                if(role == -1){
+                    et_boot_username.setText("");
+                    et_boot_password.setText("");
+                }else if(role == 1){
+                    myApplication.setUserRole("manager");
+                    System.out.println("role is manager");
+                    Intent intent = new Intent(getApplicationContext(),SelectionsActivity.class);
+                    Log.i(TAG, "login button clicked");
+                    startActivity(intent);
+                }else{
+                    myApplication.setUserRole("visitor");
+                    System.out.println("role is visitor");
+                    Intent intent = new Intent(getApplicationContext(),SelectionsActivity.class);
+                    Log.i(TAG, "login button clicked");
+                    startActivity(intent);
+                }
             }
         });
     }
